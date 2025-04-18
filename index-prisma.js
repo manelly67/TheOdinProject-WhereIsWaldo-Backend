dcf2b218dce5b30e054425bd8176db41bf93cedb
manellylopez <manellylopezqqq@gmail.com>
2025-04-18 09:20:58 -0500

    aux file for queries

--- index-prisma.js
+++ index-prisma.js
@@ -15,61 +15,6 @@ const prisma = new PrismaClient();
  
 /*  
 
-const createMany1 = await prisma.picture.createMany({
-  data: [
-    { id: 'img-1', title: 'Waldo In The Galactic City', src_image: 'https://res.cloudinary.com/dwlqplcgt/image/upload/v1743608952/find_the_charaters_in_the_galactic_city_pcuwu3.png' },
-    { id: 'img-2', title: 'Oh! Waldo is not here', src_image: 'https://res.cloudinary.com/dwlqplcgt/image/upload/v1743608911/find_the_lucky_clover_mqkdmu.png' },
-  ],
-}); 
-
-const createMany2 = await prisma.character.createMany({
-  data: [
-    { 
-      id: "char-1",
-      name: "Wally",
-      imageAboutId: 'img-1',
-      x: -0.1906976744,
-      y: 0.2805755396,
-      leftX: -0.2023255814,
-      topY: 0.2517985612,
-      rightX: -0.1860465116,
-      bottomY: 0.309352518,
-     },
-     { 
-      id: "char-2",
-      name: "R2D2",
-      imageAboutId: 'img-1',
-      x: 0.6558139535,
-      y: 0.0503597122,
-      leftX: 0.6279069767,
-      topY: -0.0107913669,
-      rightX: 0.6744186047,
-      bottomY: 0.1079136691,
-     },
-     { 
-      id: "char-3",
-      name: "Courage the Cowardly Dog",
-      imageAboutId: 'img-1',
-      x: 0.5046511628,
-      y: -0.3129496403,
-      leftX: 0.4813953488,
-      topY: -0.3525179856,
-      rightX: 0.523255814,
-      bottomY: -0.2805755396,
-     },
-     { 
-      id: "char-4",
-      name: "Four Leaf Clover",
-      imageAboutId: 'img-2',
-      x: -0.4768310912,
-      y: 0.6714285714,
-      leftX: -0.5396113602,
-      topY: 0.5964285714,
-      rightX: -0.4200298954,
-      bottomY: 0.7464285714,
-    },
-  ],
-});
 
 const all1 = await prisma.picture.findMany();
 console.log(all1);
@@ -199,13 +144,40 @@ const game = await prisma.game.update(
   },
 );
 
-*/
-let img = 'img-1';
-let player = '09856e83-11e0-4a31-981a-dd21f7555d70';
+await prisma.game.deleteMany({
+  where:{
+    AND:{
+      player:{
+        playername:{
+          equals: 'ANONIMOUS',
+        },
+      },
+      status:{
+        equals: 'ENDED',
+      },
+    }
+  },
+});
 
-/* const g = await db_games.getActiveOrEndByImgAndPlayer(img,player); */
-const g = await db_games.getActiveByImgAndPlayer(img,player);
-console.log(g);
+const search = await prisma.game.findMany({
+  where:{
+    AND:{
+      player:{
+        playername:{
+          equals: 'ANONIMOUS',
+        },
+      },
+      status:{
+        equals: 'ENDED',
+      },
+    }
+  },
+});
+
+
+*/
+
+
 
     };
 
